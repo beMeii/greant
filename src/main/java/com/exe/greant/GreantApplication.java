0ef4a94da3c949b635bfff29b5066967308f35f9
beMeii <97739731+beMeii@users.noreply.github.com>
2023-06-12 23:59:01 +0700

    init

---
+++ src/main/java/com/exe/greant/GreantApplication.java
@@ -0,0 +1,13 @@
+package com.exe.greant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exe/greant/GreantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exe.greant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
